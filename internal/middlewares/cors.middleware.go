38876fe22425fb75883bc03f45ec5275e75f71a1
nguyencaothai <tryhackme@hackme.com>
2024-09-01 09:56:54 +0700

    Update CORS

--- internal/middlewares/cors.middleware.go
+++ internal/middlewares/cors.middleware.go
@@ -1,1 +1,21 @@
 package middlewares
+
+import (
+	"github.com/anle/codebase/response"
+	"github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			response.ErrorResponseExternal(c, 20002, nil)
+			return
+		}
+
+		c.Next()
+	}
+}
