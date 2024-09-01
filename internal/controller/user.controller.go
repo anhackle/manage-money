e8ab8615029407f4887eee440bbc9c7c80839a22
nguyencaothai <tryhackme@hackme.com>
2024-09-01 14:12:34 +0700

    Update domain attribute cookie

--- internal/controller/user.controller.go
+++ internal/controller/user.controller.go
@@ -32,7 +32,7 @@ func (uc *UserController) Login(c *gin.Context) {
 
 	result, accessToken, _ := uc.userService.Login(userInput)
 	if result == response.ErrCodeSuccess {
-		c.SetCookie("access-token", accessToken, 3600, "/", "localhost", true, true)
+		c.SetCookie("access-token", accessToken, 3600, "/", "local.manage-money.com", true, true)
 	}
 
 	response.HandleResult(c, result, nil)
